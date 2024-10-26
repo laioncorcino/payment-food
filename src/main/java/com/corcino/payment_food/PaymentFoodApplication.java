daad1964ca5b8688220c0969b7e0f9f6bc3ded82
Laion Corcino <laioncorcino@gmail.com>
2024-10-26 10:10:33 -0300

    [feat] starting project

---
+++ src/main/java/com/corcino/payment_food/PaymentFoodApplication.java
@@ -0,0 +1,13 @@
+package com.corcino.payment_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corcino/payment_food/PaymentFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corcino.payment_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
